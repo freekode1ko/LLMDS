1e2c4c930afdca7aada6dcdd5efa51dfb834a90a
18933996 <oddryabkov@sberbank.ru>
2024-10-15 17:08:42 +0300

    param for count of returned docs

--- src/configs/settings.py
+++ src/configs/settings.py
@@ -3,7 +3,7 @@ from environs import Env
 env = Env()
 env.read_env()
 
-
+emd_count_docs = 4
 bot_token: str = env.str('BOT_TOKEN', default='')
 gpt_token: str = env.str('GPT_TOKEN', default='')
 elastic_password: str = env.str('ELASTIC_PASSWORD', default='')
