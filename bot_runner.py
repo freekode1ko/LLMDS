ddb284626e9f07356525e3be1d51e15fd02986fa
18933996 <oddryabkov@sberbank.ru>
2024-10-15 17:08:05 +0300

    user_param

--- bot_runner.py
+++ bot_runner.py
@@ -17,20 +17,13 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    """
-    This handler receives messages with `/start` command
-    """
     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
 
 
 @dp.message()
 async def echo_handler(message: Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
     try:
+        user = message.from_user
         if validators.url(message.text):
             print('TRUE', message.text)
         else:
