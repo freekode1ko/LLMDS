3d68443d5f38c69e1ae4567513814802ba2fcbe3
18933996 <oddryabkov@sberbank.ru>
2024-10-19 14:48:48 +0300

    upd file for config

--- src/configs/settings.py
+++ src/configs/settings.py
@@ -7,8 +7,7 @@ emd_count_docs = 4
 bot_token: str = env.str('BOT_TOKEN', default='')
 gpt_token: str = env.str('GPT_TOKEN', default='')
 elastic_password: str = env.str('ELASTIC_PASSWORD', default='')
-
-elk_index = 'llmds_storage'
-elk_url = 'https://localhost:9200'
-ca_certs = 'src/configs/ca/http_ca.crt'
+elk_url: str = env.str('ELK_URL', default='https://localhost:9200')
+elk_index: str = env.str('ELK_INDEX', default='llmds_storage')
+ca_certs: str = env.str('ELK_CERTS', default='src/configs/ca/http_ca.crt')
 
