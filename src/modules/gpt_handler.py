f6a432553e57da2a39f91a33c7f34904c5156676
Oleg Dryabkov <freekode1ko@gmail.com>
2024-10-19 20:20:31 +0300

    handler for image

--- src/modules/gpt_handler.py
+++ src/modules/gpt_handler.py
@@ -1,4 +1,5 @@
 import logging
+import base64
 from openai import OpenAI
 from src.configs.settings import gpt_token, gpt_model
 
@@ -23,7 +24,6 @@ def ask_gpt_about_fragment(fragment: str, query: str) -> str:
             temperature=0.0,
             max_tokens=300
         )
-        # Извлекаем текст ответа из структуры ответа правильно
         return response.choices[0].message.content.strip()
     except Exception as e:
         logging.error(f"Ошибка при запросе к GPT: {e}")
@@ -57,3 +57,38 @@ def summarize_answers(answers: list, query: str) -> str:
     except Exception as e:
         logging.error(f"Ошибка при суммаризации ответа с GPT: {e}")
         return "Не удалось создать суммарный ответ."
+
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+
+def ask_gpt_about_image(image_path: str, query: str) -> str:
+    """
+    Отправляет изображение в GPT и задает вопрос, возвращая ответ.
+    """
+    try:
+        print('преобразование')
+        base64_image = encode_image(image_path)
+        print('преобразование готово')
+        response = client.chat.completions.create(
+            model="gpt-4-vision",
+            messages=[
+                {"role": "system",
+                 "content": "Ты - помощник, который суммирует ответы по запросу пользователя и предоставляет "
+                            "максимально развернутый и детализированный ответ."},
+                {"role": "user", "content":
+                    [{"type": "text", "text": query, },
+                     {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"}}]
+                 }
+            ],
+            temperature=0.0,
+            max_tokens=1000
+        )
+        print('ответ получен')
+        return response.choices[0].message.content.strip()
+    except Exception as e:
+        print('ERROR!')
+        logging.error(f"Ошибка при обработке изображения с GPT: {e}")
+        return "Не удалось получить ответ на основе изображения."
